74c88c811b85a9759e2b368f406f32c16ac47c1d
iaacornus <iaacornus.devel@gmail.com>
2022-12-13 14:24:40 +0800

    update conditions on breaking the loop

--- scripts/battery-threshold.sh
+++ scripts/battery-threshold.sh
@@ -2,13 +2,13 @@
 
 battery_level=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep 'percentage:' | awk '{ print $2 }' | sed 's/%//')
 state=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep 'state:' | awk '{ print $2 }' | sed 's/%//')
-if [[ "$state" == "charging" ]] && [[ battery_level -eq 55 ]]; then
+if [[ "$state" == "charging" ]] && [[ battery_level -eq 79 ]]; then
     notify-send "Battery" "Threshold of 80% reached. Unplug the charger."
     paplay /usr/share/sounds/Yaru/stereo/complete.oga
 
     while true; do # to avoid sending a lot of notifications and sound
         sleep 15 # prevent from running too often
-        if [[ battery_level=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep 'percentage:' | awk '{ print $2 }' | sed 's/%//') -ne 55 ]]; then
+        if [[ battery_level=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep 'percentage:' | awk '{ print $2 }' | sed 's/%//') -lt 79 ]]; then
             break
         fi
     done
