2e94152c0940a817ef91f3b055ca5d3e374e4629
iaacornus <iaacornus.devel@gmail.com>
2022-12-13 14:07:31 +0800

    improve conditions for checking

--- scripts/battery-threshold.sh
+++ scripts/battery-threshold.sh
@@ -2,12 +2,13 @@
 
 battery_level=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep 'percentage:' | awk '{ print $2 }' | sed 's/%//')
 state=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep 'state:' | awk '{ print $2 }' | sed 's/%//')
-if [[ "$state" == "discharging" ]] && [[ battery_level -eq 50 ]]; then
+if [[ "$state" == "charging" ]] && [[ battery_level -eq 79 ]]; then
     notify-send "Battery" "Threshold of 80% reached. Unplug the charger."
     paplay /usr/share/sounds/Yaru/stereo/complete.oga
 
     while true; do # to avoid sending a lot of notifications and sound
-        if [[ $(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep 'percentage:' | awk '{ print $2 }' | sed 's/%//') -lt 50 ]]; then
+        state=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep 'state:' | awk '{ print $2 }' | sed 's/%//')
+        if [[ "$state" == "discharging" ]]; then
             break
         fi
         sleep 15 # prevent from running too often
