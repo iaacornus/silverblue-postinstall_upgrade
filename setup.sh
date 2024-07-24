9df997f056ea017f7badf84e685788349c16a508
James Aaron Erang <iaacornus.devel@gmail.com>
2024-07-24 20:44:46 +0800

    help message and some basic functions

--- setup.sh
+++ setup.sh
@@ -7,7 +7,41 @@
 #
 # ---------------------------------
 
+
+FAIL="\e[1;41m[ FAIL ]\e[0m"
+SUCCESS="\e[1;42m[ SUCCESS ]\e[0m"
+INFO="\e[1;44m[ INFO ]\e[0m"
+__USAGE="
+Usage: bash setup.sh [OPTIONS]
+
+Options:
+    -h, --help                  Print this help message.
+    -a, --all                   Apply all of the recommended modifications on the system.
+    -f, --flatpak               Setup flatpak repository.
+    -r, --rpmfusion             Setup RPMFusion repository.
+    -c, --codecs                Install codecs.
+    -d, --driver                Install appropriate drivers.
+    -o, --optimize              Perform suggested system optimizations.
+    -l, --laptop                Apply laptop recommendations.
+    -n, --nvidia                Install proprietary NVidia drivers.
+"   
+
+function setup_fail () {
+    echo -e "$FAIL Installation failed for some reason."
+    exit 1
+}
+
+function setup_success () {
+    echo -e "$SUCCESS Installation successfully finished."
+    exit 0
+}
+
+function print_help () {
+
+}
+
 function main () {
+
 
 }
 
