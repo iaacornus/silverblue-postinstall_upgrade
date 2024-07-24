51b65b91f432c99261ea30a2d0e1ca736f952f26
James Aaron Erang <iaacornus.devel@gmail.com>
2024-07-24 20:36:03 +0800

    initial setup script for automated post install setup

---
+++ setup.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# ---------------------------------
+#
+# Fedora Silverblue Post Install Script
+# Copyright (c) 2024 James Aaron Erang <iaacornus@gmail.com>
+#
+# ---------------------------------
+
+function main () {
+
+}
+
+function setup_flatpak () {
+
+}
+
+function setup_rpmfusion () {
+
+}
+
+function install_codecs () {
+
+}
+
+function install_nvidia () {
+
+
+}
+
+
+function sys_opts () {
+
+
+}
+
+function laptop_setup () {
+
+
+}
